c12f6be7db377c50563c31f985ecd467e121f0d7
Saif Ali <83064362+itsBudhhu@users.noreply.github.com>
2022-03-28 20:17:07 +0530

    Update KarpitSpamBot.py

--- KarpitSpamBot.py
+++ KarpitSpamBot.py
@@ -6,7 +6,7 @@ from os import execl
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.functions.account import UpdateProfileRequest
-from Config import STRING, SUDO, BIO_MESSAGE, ALIVE_IMAGE, ALIVE_NAME, API_ID, API_HASH, HEROKU_APP_NAME, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12 , STRING13 , STRING14 , STRING15 ,STRING16 , STRING17 , STRING18 , STRING19 , STRING20 , STRING21 , STRING22 , STRING23 , STRING24 , STRING25 
+from Config import STRING, SUDO, BIO_MESSAGE, ALIVE_IMAGE, ALIVE_NAME, API_ID, API_HASH, HEROKU_APP_NAME, HEROKU_API_KEY, STRING2, STRING3, STRING4 ,STRING5, STRING6, STRING7, STRING8 ,STRING9, STRING10, STRING11, STRING12 , STRING13 , STRING14 , STRING15 ,STRING16 , STRING17 , STRING18 , STRING19 , STRING20 , STRING21 , STRING22 , STRING23 , STRING24 , STRING25 
 import asyncio
 import telethon.utils
 from telethon.tl import functions
@@ -14,6 +14,14 @@ from telethon.tl.functions.channels import LeaveChannelRequest
 from telethon.tl.functions.messages import ImportChatInviteRequest
 from Utils import RAID, RRAID, MRAID, SRAID, JRAID, PORNSPAM, CRAID
 from telethon.tl.functions.channels import JoinChannelRequest
+from telethon.tl.functions.users import GetFullUserRequest
+
+import heroku3
+
+Heroku = heroku3.from_key(HEROKU_API_KEY)
+heroku_api = "https://api.heroku.com"
+sudousers = os.environ.get("SUDO", None)
+
 
 
 a = API_ID
